a0271a8434386c0cc1475a8d86f38a8d0be0c3d1
Shayne Maravillo <yashnedobasa@gmail.com>
2019-01-09 14:15:28 +0800

    AdvanceWebJakeCarlosRenzo

---
+++ AdvanceWebJakeRenzoCarlos.Web/ViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AdvanceWebJakeRenzoCarlos.Web.Infrastructures.Data.Helpers;
+using AdvanceWebJakeRenzoCarlos.Web.Infrastructures.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvanceWebJakeRenzoCarlos.Web.ViewModels
+{
+    public class IndexViewModel
+    {
+        public Page<User> Users { get; set; }
+    }
+}
